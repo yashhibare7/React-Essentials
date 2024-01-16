cf8671efc942e55da229481c39f9efe41a277ce2
yhibare <96299060+yashhibare7@users.noreply.github.com>
2024-01-17 01:26:15 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js'
 
 function App() {
+  const [ selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    //console.log(selectedTopic)
+  }
+
+  console.log('APP COMPONENT EXECUTING');
+
   return (
     <div>
       <Header />
@@ -24,11 +37,21 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+            {!selectedTopic && <p>Please select a topic.</p>}
+            {selectedTopic && (
+              <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+              </div>
+            )}
         </section>
       </main>
     </div>
